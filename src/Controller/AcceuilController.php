490543fd4c4b7bc157f134486a5a4aa274631db2
Thomas <spacelord559@hotmail.com>
2025-09-12 15:32:43 +0200

    phase 1 : creation de projet. phase 2 : premier test "hello world !"

---
+++ src/Controller/AcceuilController.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+/**
+ * Description of AcceuilController
+ *
+ * @author Thomas
+ */
+class AcceuilController {
+    
+    #[Route('/', name: 'accueil')]
+    public function index (): Response{
+        return new Response ('hello world !');
+        
+    }
+}

a2564f7727e084903321527a14485219768c44b4
Thomas <spacelord559@hotmail.com>
2025-09-13 14:18:13 +0200

    phase 1 : creation de projet. phase 2 : premier test "hello world !" pahse 3 : utilisation de Twig

--- src/Controller/AcceuilController.php
+++ src/Controller/AcceuilController.php
@@ -7,14 +7,16 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 /**
  * Description of AcceuilController
  *
  * @author Thomas
  */
-class AcceuilController {
+class AcceuilController extends AbstractController {
     
     #[Route('/', name: 'accueil')]
     public function index (): Response{

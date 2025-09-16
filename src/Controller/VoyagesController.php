4b95995268124942b4c522ec3a89ede3b7712029
Thomas <spacelord559@hotmail.com>
2025-09-16 15:41:33 +0200

    phase 1 : creation de projet. phase 2 : premier test "hello world !" pahse 3 : utilisation de Twig Phase 5 : bootstrap (2 pages et une barre de navigation) "Phase 7 : ORM (affichage des informations provenant de la BDD)." Phase 8 : ORM (tri des enregistrements). Phase 9 : ORM (filtrage des enregistrements). Phase 10 : ORM (affichage détaillé d'une visite)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -37,6 +37,14 @@ class VoyagesController extends AbstractController {
         ]);
         
     }
+    #[Route('/voyages/voyage/{id}', name:'voyage.showone')]
+    public function showOne($id) : Response {
+        $visite = $this->repository->find($id);
+        return $this->render("pages/voyage.html.twig",[
+           'visite' => $visite 
+        ]);
+        
+    }
     /**
     * 
     * @var VisiteRepository

ea3bff2e12c25c01c98259b0feb8096298e7cb4b
Thomas <spacelord559@hotmail.com>
2025-09-17 12:21:34 +0200

    Phase 14 : ORM (ajout table et lien entre tables)

--- src/Controller/admin/AdminVoyagesController.php
+++ src/Controller/admin/AdminVoyagesController.php
@@ -62,5 +62,20 @@ public function edit(int $id,Request $request): Response {
         'formvisite' => $formVisite->createView()
       ]);
 }
+#[Route('/admin/ajout', name: 'admin.voyage.ajout')]
+public function ajout(Request $request): Response {
+    $visite = new Visite();
+    $formVisite = $this->createForm(VisiteType::class, $visite);
+    
+    $formVisite->handleRequest($request);
+    if($formVisite->isSubmitted() && $formVisite->isValid()){
+        $this->repository->add($visite);
+        return $this->redirectToRoute('admin.voyages');
+    }
+      return $this->render("admin/admin.voyage.ajout.html.twig", [
+        'visite' => $visite,
+        'formvisite' => $formVisite->createView()
+      ]);
+}
 }
 
